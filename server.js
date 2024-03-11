8b1f0092043ae68d889f305005d40104c57a2d9f
laylinessteve <steven.lee.anderson@gmail.com>
2024-03-11 12:46:41 -0500

    Setup basic server and fixed issues

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('AI Fitness Coach API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
